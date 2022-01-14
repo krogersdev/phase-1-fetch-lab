8d17ee13d21e554c5579d724581f74ed09c272af
Kevin Rogers <kevinarogers1@gmail.com>
2022-01-14 11:58:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  let fetchData = fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(json => renderBooks(json))
+    
+  return fetchData;       
 }
 
 function renderBooks(books) {
